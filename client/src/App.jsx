ed3a68d37cff71442643b018f4fae20aee5f0ff4
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-02-06 21:17:45 +0300

    add tooggle menu functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Globals.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './Components/Header/Header';
 import Records from './Pages/Records/Records';
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import  { Link } from 'react-router-dom'
+import { FaHospitalSymbol } from "react-icons/fa";
 import './Header.css'
-function Header() {
+function Header(icon) {
+
+  function handleToggleNav(){
+const navigationIcon = document.getElementById('nav')
+navigationIcon.classList.toggle('open')
+navigationIcon.classList.add("close")
+// const openNav = document.getElementById("#nav")
+// openNav.classList.toggle("close")
+
+
+
+  }
   return (
     <header className='app-header'>
         <h3>Lorem ipsum dolor <span className='span'>sit amet</span></h3>
@@ -15,6 +27,18 @@ function Header() {
         <li><Link to={'/'}>Logout</Link></li>
         </ul>
 </nav>
+
+<div id='nav-icon'>
+<p onClick={handleToggleNav}><FaHospitalSymbol/></p>
+<nav id='nav'>
+        <ol className='nav-list'>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/Records'}>My Records</Link></li>
+        <li><Link to={'/Profile'}>Profile</Link></li>
+        <li><Link to={'/'}>Logout</Link></li>
+        </ol>
+</nav>
+</div>
     </header>
   )
 }
