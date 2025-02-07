ec8b25d372c0418994de8f4a4924fd7befdc3fd5
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-02-07 21:36:50 +0300

    validate further

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -32,6 +32,8 @@ diastoric:"",
       if(!values.oxygenSaturation) errors.oxygenSaturation = "oxygenSaturation is required"
       if(!values.ejectionFraction) errors.ejectionFraction = "AejectionFractionis required"
       if(!values.weight) errors.weight = "weight is required"
+      if(values.weight <1)errors.weight = "weight must be greater than 1Kg"
+      if(values.weight >150) errors.weight = "Weight must not exceed 150Kg"
       if(!values.choresterol) errors.choresterol = "choresterol is required"
       if(!values.height) errors.height = "height is required"
       if(!values.heartRate) errors.heartRate = "heartRate is required"
@@ -39,13 +41,15 @@ diastoric:"",
       if(!values.murmur) errors.murmur ="murmur is required"
       if(!values.systolic) errors.systolic = "systolic is required"
       if(!values.diastoric) errors.diastoric = "diastoric is required"
+
+
       return errors;
     }
     
   })
   return (
    <>
-   {/* <h3  className='home-heading'>Test your <span> VSD </span> status here</h3> */}
+   <h3  className='home-heading'>Test your <span> VSD </span> status here</h3>
     <div className='home' id='home'>
 <section className='home-section'> 
 <div className='result-area'>
@@ -83,8 +87,11 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.age}
+    required
+    max={150}
+    min={0}
     />
-    {formik.touched.age && formik.errors.age && (<p>{formik.errors.age}</p>)}
+    {formik.touched.age && formik.errors.age && (<p  className='errors'>{formik.errors.age}</p>)}
     </div>
 
     <div className='home-from-inputs'>
@@ -94,6 +101,7 @@ diastoric:"",
      onChange={formik.handleChange}
      onBlur={formik.handleBlur}
      value={formik.values.gender}
+     required
      >
 
       <option value="Male">Male</option>
@@ -109,6 +117,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.oxygenSaturation}
+    required
+    max={100}
+    min={1}
     />
     {formik.touched.oxygenSaturation && formik.errors.oxygenSaturation && (<p  className='errors'>{formik.errors.oxygenSaturation}</p>)}
     </div>
@@ -122,6 +133,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.ejectionFraction}
+    required
+    max={100}
+    min={1}
     />
     {formik.touched.ejectionFraction && formik.errors.ejectionFraction && (<p  className='errors'>{formik.errors.ejectionFraction}</p>)}
     </div>
@@ -134,6 +148,9 @@ diastoric:"",
      onChange={formik.handleChange}
      onBlur={formik.handleBlur}
      value={formik.values.weight}
+     required
+     max={250}
+     min={2}
      />
      {formik.touched.weight && formik.errors.weight && (<p  className='errors'>{formik.errors.weight}</p>)}
     </div>
@@ -146,6 +163,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.choresterol}
+    required
+    max={1}
+    min={0}
     />
     {formik.touched.choresterol && formik.errors.choresterol && (<p  className='errors'>{formik.errors.choresterol}</p>)}
     </div>
@@ -160,6 +180,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.height}
+    required
+    max={250}
+    min={30}
     />
     {formik.touched.height && formik.errors.height && (<p  className='errors'>{formik.errors.height}</p>)}
     </div>
@@ -172,6 +195,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.heartRate}
+    required
+    max={200}
+    min={10}
     />
     {formik.touched.heartRate && formik.errors.heartRate && (<p  className='errors'>{formik.errors.heartRate}</p>)}
     </div>
@@ -184,6 +210,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.cyanosis}
+    required
+    max={1}
+    min={0}
     />
     {formik.touched.cyanosis && formik.errors.cyanosis && (<p  className='errors'>{formik.errors.cyanosis}</p>)}
     </div>
@@ -196,6 +225,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.murmur}
+    required
+    max={1}
+    min={0}
     />
     {formik.touched.murmur && formik.errors.murmur && (<p  className='errors'>{formik.errors.murmur}</p>)}
     </div>
@@ -208,6 +240,9 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.systolic}
+    required
+    max={259}
+    min={0}
 
     />
     {formik.touched.systolic && formik.errors.systolic && (<p  className='errors'>{formik.errors.systolic}</p>)}
@@ -223,6 +258,8 @@ diastoric:"",
     onChange={formik.handleChange}
     onBlur={formik.handleBlur}
     value={formik.values.diastoric}
+    required
+    
     />
     {formik.touched.diastoric && formik.errors.diastoric && (<p  className='errors'>{formik.errors.diastoric}</p>)}
     </div>
