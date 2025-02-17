374f627942f6f6f4d711d52261850b08832088f9
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-02-17 21:48:45 +0300

    validate form

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { useFormik } from 'formik'
+import { useFormik, Field } from 'formik'
 import './profile.css'
 import useUserStore from '../../../Store/userStore'
 
@@ -34,6 +34,19 @@ function Profile() {
       
     },
     onSubmit:handleSubmit,
+    validate:function(values){
+const errors = {}
+if(!values.firstname) errors.firstname="first Name Is required";
+if(!values.lastname) errors.lastname="Last name is required";
+if(!values.email) {errors.email= "email is required"} 
+if(!values.phone) errors.phone="phone number is require";
+if(!values.maritual_status) errors.maritual_status="Maritual staatus is required"
+if(!values.disability) errors.maritual_status = "maritual status is required"
+if(!values.gender) errors.gender = "gender is required"
+if(!values.next_of_kin) errors.next_of_kin ="next of kin is required"
+if(!values.next_of_kin_phone) errors.next_of_kin_phone = "next of kin phone is required"
+return errors
+    }
   })
   
   return (
@@ -49,17 +62,33 @@ function Profile() {
         <div className='profile_form_fields'>
             <div className='profile_inputs'>
                 <label htmlFor="firstname">first name *</label>
-                <input type="text" onChange={formik.handleChange } onblur={formik.handleBlur}  value={formik.values.firstname}/>
+                <input 
+                type="text" 
+                name='firstname'
+                value={formik.values.firstname}
+                onChange={formik.handleChange } 
+                onBlur={formik.handleBlur}  
+                />
+                {formik.touched.firstname && formik.errors.firstname && (<p className='errors'>{formik.errors.firstname}</p>)}
             </div>
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">last name *</label>
-                <input type="text"  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.lastname} />
+                <input type="text" name='lastname' onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.lastname} />
+                {formik.touched.lastname && formik.errors.lastname && (<p className='errors'>{formik.errors.lastname}</p>)}
             </div>
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">email *</label>
-                <input type="email"  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.email} />
+                <input 
+    type="text" 
+    placeholder='enter your email'
+    name='email'
+    value={formik.values.email}
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    required />
+    {formik.touched.email && formik.errors.email && (<p className='errors'>{formik.errors.email}</p>)}
             </div>
         </div>
         {/* ************************************************* */}
@@ -67,25 +96,27 @@ function Profile() {
         <div className='profile_form_fields'>
             <div className='profile_inputs'>
                 <label htmlFor="firstname">phone number *</label>
-                <input type="number"  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.phone} />
+                <input type="number" name='phone'  onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.phone} />
+                {formik.touched.phone && formik.errors.phone && (<p className='errors'>{formik.errors.phone}</p>)}
             </div>
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">marital status *</label>
-                <select name="" id=""  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.maritual_status}>
+                <select  id=""  onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.maritual_status}>
+                  {formik.touched.maritual_status && fromik.errors.maritual_status &&(<p className='errors'> {formik.errors.firstname}</p>)}
                   <option value="">select</option>
-                <option value="">Single</option>
-                  <option value="">Married</option>                 
-                  <option value="">Divorced</option>
+                <option value="single">Single</option>
+                  <option value="married">Married</option>                 
+                  <option value="divorced">Divorced</option>
                 </select>
             </div>
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">any dissability *</label>
-                <select name="" id=""  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.disability}>
+                <select name="disability" id=""  onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.disability}>
                 <option value="">select </option>
-                <option value="">No</option>
-                  <option value="">yes</option>                 
+                <option value="no">No</option>
+                  <option value="yes">yes</option>                 
                 </select>
             </div>
         </div>
@@ -94,7 +125,7 @@ function Profile() {
         <div className='profile_form_fields'>
             <div className='profile_inputs'>
                 <label htmlFor="firstname">gender *</label>
-                <select name="" id=""  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.gender}>
+                <select name="" id=""  onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.gender}>
                 <option value="">select </option>
                 <option value="">Male</option>
                   <option value="">Female</option>                 
@@ -103,12 +134,14 @@ function Profile() {
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">next of kin name *</label>
-                <input type="text"  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.next_of_kin} />
+                <input type="text" name='next_of_kin' onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.next_of_kin} />
+                {formik.touched.next_of_kin && formik.errors.next_of_kin && (<p className='errors'>{formik.errors.next_of_kin}</p>)}
             </div>
 
             <div className='profile_inputs'>
                 <label htmlFor="firstname">next of kin phone number *</label>
-                <input type="number"  onChange={formik.handleChange } onblur={formik.handleBlur} value={formik.values.next_of_kin_phone} />
+                <input type="number"name='next_of_kin_phone'  onChange={formik.handleChange } onBlur={formik.handleBlur} value={formik.values.next_of_kin_phone} />
+                {formik.touched.next_of_kin_phone && formik.errors.next_of_kin_phone && (<p className='errors'>{formik.errors.next_of_kin_phone}</p>)}
             </div>
             
         </div>
