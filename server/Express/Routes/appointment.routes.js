d1536dbb7a839243de13a7ffb578d37d93120e37
unknown <njerivictory52@gmail.com>
2025-03-11 18:39:50 +0300

    Create Patch API endpoint

--- server/Express/Routes/appointment.routes.js
+++ server/Express/Routes/appointment.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createAppointment,getApppointment } from "../Controllers/schedules.controllers.js";
+import { createAppointment,getApppointment, updateAppointment} from "../Controllers/schedules.controllers.js";
 
 const router=Router()
 router.post("/bookAppointment",createAppointment)
 router.get("/viewAppointment",getApppointment)
+router.patch("/editAppointment/:id",updateAppointment)
 
 export default  router;
